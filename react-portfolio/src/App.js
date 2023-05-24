edaee3a86040454e87134dcb6e314755a86ea0c9
Jacob Templeton <jacob.templeton@gmail.com>
2023-05-24 16:23:34 -0700

    Add image to about section, image styling

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -4,7 +4,7 @@ import About from "./components/About";
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Hero />
       <About />
     </div>
--- react-portfolio/src/components/About.jsx
+++ react-portfolio/src/components/About.jsx
@@ -1,7 +1,16 @@
+import "./About.css";
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="about-section">
+      <div className="about-img-section">
+        <img
+          className="about-img"
+          src="./jwct-profile-pic.jpeg"
+          alt="profile-pic"
+        />
+      </div>
+      <div className="about-text-section">text</div>
     </div>
   );
 };
