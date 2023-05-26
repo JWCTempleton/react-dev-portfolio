fb8b143fc9dc863e78ba3b192ddd75c003e6be37
Jacob Templeton <jacob.templeton@gmail.com>
2023-05-25 17:19:40 -0700

    Add projects section

--- react-portfolio/src/components/Projects.jsx
+++ react-portfolio/src/components/Projects.jsx
@@ -1,11 +1,17 @@
-import "./Projects.css"
+import "./Projects.css";
 
 const Projects = () => {
-    return (
-        <div className="projects-section">
-            <h1>Projects</h1>
+  return (
+    <div className="projects-section">
+      <h2>My Projects</h2>
+      <div className="projects-content">
+        <div className="projects-text">Text</div>
+        <div className="projects-image">
+          <img className="projects-photo" src="./laptop-1.jpg" alt="laptop" />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
