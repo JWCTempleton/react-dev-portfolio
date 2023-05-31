a843b8fdd986aea6f7da8428dc290eda9982afbb
Jacob Templeton <jacob.templeton@gmail.com>
2023-05-30 21:30:56 -0700

    Add call to action segment, styling

--- react-portfolio/src/components/Cta.jsx
+++ react-portfolio/src/components/Cta.jsx
@@ -1,3 +1,17 @@
-const Cta = () => <div>Call to action</div>;
+import "./Cta.css";
+import { Fade } from "react-awesome-reveal";
+
+const Cta = () => (
+  <div className="call">
+    <Fade cascade damping={0.2} duration={2500}>
+      <h2 className="call-title">Contact</h2>
+
+      <p className="call-content">
+        Please feel free to contact me, I'd love to talk more.
+      </p>
+      <p className="call-content">jacob.templeton@gmail.com</p>
+    </Fade>
+  </div>
+);
 
 export default Cta;
