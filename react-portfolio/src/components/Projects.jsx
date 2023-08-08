980b204ac345934a65582b37bcdc0670ab425b64
Jacob Templeton <jacob.templeton@gmail.com>
2023-08-07 18:11:20 -0700

    Add information about final project in Projects component

--- react-portfolio/src/components/Projects.jsx
+++ react-portfolio/src/components/Projects.jsx
@@ -124,7 +124,48 @@ const Projects = ({ darkMode }) => {
       </Fade>
       <Fade duration={1200}>
         <div className="projects-content mb-medium">
-          <div className="projects-text">Text</div>
+          <div className="projects-text">
+            <a href="https://jwctempleton.github.io/hacker-news-search/">
+              <h2>HackerNews Search</h2>
+            </a>
+            <p>
+              A React project focused on incorporating API data to the front
+              end. Axios library used to fetch data, async/wait functions and
+              advanced state handling using reducers round out this project.
+            </p>
+            <a href="https://github.com/JWCTempleton/hacker-news-search">
+              <p
+                style={{
+                  display: "flex",
+                  gap: ".4rem",
+                  marginTop: ".8rem",
+                  fontWeight: "900",
+                  alignItems: "center",
+                }}
+              >
+                Github Code
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon icon-tabler icon-tabler-brand-github-filled"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  strokeWidth="2"
+                  stroke="currentColor"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path
+                    d="M5.315 2.1c.791 -.113 1.9 .145 3.333 .966l.272 .161l.16 .1l.397 -.083a13.3 13.3 0 0 1 4.59 -.08l.456 .08l.396 .083l.161 -.1c1.385 -.84 2.487 -1.17 3.322 -1.148l.164 .008l.147 .017l.076 .014l.05 .011l.144 .047a1 1 0 0 1 .53 .514a5.2 5.2 0 0 1 .397 2.91l-.047 .267l-.046 .196l.123 .163c.574 .795 .93 1.728 1.03 2.707l.023 .295l.007 .272c0 3.855 -1.659 5.883 -4.644 6.68l-.245 .061l-.132 .029l.014 .161l.008 .157l.004 .365l-.002 .213l-.003 3.834a1 1 0 0 1 -.883 .993l-.117 .007h-6a1 1 0 0 1 -.993 -.883l-.007 -.117v-.734c-1.818 .26 -3.03 -.424 -4.11 -1.878l-.535 -.766c-.28 -.396 -.455 -.579 -.589 -.644l-.048 -.019a1 1 0 0 1 .564 -1.918c.642 .188 1.074 .568 1.57 1.239l.538 .769c.76 1.079 1.36 1.459 2.609 1.191l.001 -.678l-.018 -.168a5.03 5.03 0 0 1 -.021 -.824l.017 -.185l.019 -.12l-.108 -.024c-2.976 -.71 -4.703 -2.573 -4.875 -6.139l-.01 -.31l-.004 -.292a5.6 5.6 0 0 1 .908 -3.051l.152 -.222l.122 -.163l-.045 -.196a5.2 5.2 0 0 1 .145 -2.642l.1 -.282l.106 -.253a1 1 0 0 1 .529 -.514l.144 -.047l.154 -.03z"
+                    strokeWidth="0"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </p>
+            </a>
+          </div>
 
           <div className="projects-image mb-medium">
             <Tilt
@@ -134,11 +175,13 @@ const Projects = ({ darkMode }) => {
               glarePosition="bottom"
               glareBorderRadius="20px"
             >
-              <img
-                className="projects-photo"
-                src="./laptop-3.jpg"
-                alt="laptop"
-              />
+              <a href="https://jwctempleton.github.io/hacker-news-search/">
+                <img
+                  className="projects-photo"
+                  src="./laptop-3.jpg"
+                  alt="laptop"
+                />
+              </a>
             </Tilt>
           </div>
         </div>
